cd9d09e63fdafc8523797941760b6bef01f9f032
Jennifer <yortree@gmail.com>
2015-05-20 16:03:56 -0700

    Add several scenerios and routing options.

--- app.rb
+++ app.rb
@@ -19,3 +19,35 @@ get('/carnival') do
   @player_name = User.all().name()
   erb(:carnival)
 end
+
+get('/a1') do
+  @player_name = User.all().name()
+  erb(:a1)
+end
+
+get('/a2') do
+  @player_name = User.all().name()
+  erb(:a2)
+end
+
+get('/a3') do
+  @player_name = User.all().name()
+  erb(:a3)
+end
+
+get('/a4') do
+  @player_name = User.all().name()
+  erb(:a4)
+end
+
+
+get('/a6') do
+  @player_name = User.all().name()
+  erb(:a6)
+end
+
+
+get('/a7') do
+  @player_name = User.all().name()
+  erb(:a6)
+end
