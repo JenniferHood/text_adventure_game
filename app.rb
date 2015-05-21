7add82c69cc486d065c670ee7fc3b3ff6a837592
J0hnnyWhy <jayelwhy@yahoo.com>
2015-05-21 10:35:07 -0700

    john's C1

--- app.rb
+++ app.rb
@@ -19,3 +19,58 @@ get('/carnival') do
   @player_name = User.all().name()
   erb(:carnival)
 end
+
+get('/city') do
+  @player_name = User.all().name()
+  erb(:city)
+end
+
+get('/c1') do
+  @player_name = User.all().name()
+  erb(:c1)
+end
+
+get('/c2') do
+  @player_name = User.all().name()
+  erb(:c2)
+end
+
+get('/c3') do
+  @player_name = User.all().name()
+  erb(:c3)
+end
+
+get('/c4') do
+  @player_name = User.all().name()
+  erb(:c4)
+end
+
+get('/c5') do
+  @player_name = User.all().name()
+  erb(:c5)
+end
+
+get('/c6') do
+  @player_name = User.all().name()
+  erb(:c6)
+end
+
+get('/c7') do
+  @player_name = User.all().name()
+  erb(:c7)
+end
+
+get('/c8') do
+  @player_name = User.all().name()
+  erb(:c8)
+end
+
+get('/c9') do
+  @player_name = User.all().name()
+  erb(:c9)
+end
+
+get('/c10') do
+  @player_name = User.all().name()
+  erb(:c10)
+end
